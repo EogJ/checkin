270f836e3ebd0cd280ebb4fa289d48d130972c13
Eoghain Johnson <eoghain.johnson@gmail.com>
2023-05-31 17:37:02 +1000

    add todos

--- checkin.go
+++ checkin.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
 func main() {
-	http.HandleFunc("/", entries)
+	http.HandleFunc("/", todos)
 	http.ListenAndServe(":8080", nil)
 }
 
-func entries(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
+func todos(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	data := TodoPageData{
+		PageTitle: "Todos",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(w, data)
 }
